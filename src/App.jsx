e123a7cc8d72482a7774039a47632729ce5ec759
ShoheiTakei <mrshohei195@gmail.com>
2022-01-22 16:34:06 +0900

    最低限の要素でそれっぽく作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './Router/Router';
+import { GlobalStyle } from './Style/GlobalStyle';
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Router />
     </BrowserRouter>
   );
--- src/components/pages/IntroPage.jsx
+++ src/components/pages/IntroPage.jsx
@@ -1,39 +1,57 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 export const IntroPage = () => {
+  const navigate = useNavigate();
+
   return (
     <LayoutStyle>
-      <h1>Seen</h1>
+      <h1>
+        <span>S</span>een
+      </h1>
       <p>
         気になるけど、まだ見ていない映画はありますか？
-        見たいと感じたら、忘れてしまう前にマイリストに追加しましょう！
+        <br />
+        <br />
+        見たいと感じたら、忘れてしまう前にマイリストに追加しましょう!
       </p>
-      <Button variant="contained">利用する</Button>
+      <Button
+        size="large"
+        variant="outlined"
+        color="primary"
+        onClick={() => {
+          navigate('/home');
+        }}
+      >
+        利用する
+      </Button>
     </LayoutStyle>
   );
 };
 
 const LayoutStyle = styled.div`
   color: white;
-  background: #060606de;
-  width: 100%;
-  height: 100%;
   text-align: center;
   h1 {
     color: #5adaf6;
-    font-family: Rounded Mplus 1c Bold;
-    font-size: 14.5rem;
+    font-family: RoundedMplus1cBold;
+    font-size: 13rem;
     text-decoration-line: underline;
-    text-transform: capitalize;
+    margin: 4rem 0;
+    span {
+      color: #fb6a00;
+    }
   }
   p {
-    font-family: Rounded Mplus 1c Bold;
+    font-family: RoundedMplus1cBold;
     font-style: normal;
     font-weight: bold;
-    font-size: 36px;
-    line-height: 53px;
+    font-size: 2rem;
+    line-height: 2rem;
     text-align: center;
     text-transform: capitalize;
+    margin: 5rem 0;
   }
 `;
